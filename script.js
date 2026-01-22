eaf7fcc4556e3079872df36899a38ced4b2ff2a9
srima13052003-collab <srima13052003@gmail.com>
2026-01-22 19:13:01 +0530

    Implement addToCart function for shopping cart
    Added function to add items to the shopping cart and store in localStorage.

--- script.js
+++ script.js
@@ -40,3 +40,16 @@ loadSingleProduct(
   "Veloura Earrings",
   "Earrings.html"
 );
+function addToCart(name, price, image) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({
+    name: name,
+    price: price,
+    image: image
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert(`${name} added to cart`);
+}
