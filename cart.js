f7abaa4b0766d8604e7bf0db567cc847196eba89
srima13052003-collab <srima13052003@gmail.com>
2026-01-22 19:57:34 +0530

    Implement cart display and total calculation
    Initialize cart from localStorage and display items with total price.

---
+++ cart.js
@@ -0,0 +1,23 @@
+   let cart = JSON.parse(localStorage.getItem("cart")) || [];
+const cartContainer = document.getElementById("cart-items");
+let total = 0;
+
+cart.forEach(item => {
+  total += item.price;
+
+  const card = document.createElement("div");
+  card.className = "product-card";
+
+  card.innerHTML = `
+    <h3>${item.name}</h3>
+    <p>₹${item.price}</p>
+  `;
+
+  cartContainer.appendChild(card);
+});
+
+document.getElementById("total-price").innerText = `Total: ₹${total}`;
+
+function checkoutUPI() {
+  alert("UPI payment will be added next");
+}
